ed07f2dd53fdcc00917f52a0a90339585de7b787
Lukas Kouril <kouril.lukas@gmail.com>
2025-10-12 20:05:30 +0200

    skills order change

--- src/app/components/hero/Skills.js
+++ src/app/components/hero/Skills.js
@@ -4,17 +4,17 @@ import { Card, CardContent } from "@/components/ui/card";
 import { motion } from "framer-motion";
 
 const skills = [
-  { name: "HTML5", icon: "devicon-html5-plain colored" },
-  { name: "CSS3", icon: "devicon-css3-plain colored" },
   { name: "JavaScript", icon: "devicon-javascript-plain colored" },
   { name: "TypeScript", icon: "devicon-typescript-plain colored" },
-  { name: "React", icon: "devicon-react-original colored" },
-  { name: "Next.js", icon: "devicon-nextjs-plain" },
   { name: "Node.js", icon: "devicon-nodejs-plain colored" },
-  { name: "Vue.js", icon: "devicon-vuejs-plain colored" },
-  { name: "TailwindCSS", icon: "devicon-tailwindcss-original colored" },
   { name: "MySQL", icon: "devicon-mysql-original colored" },
   { name: "PostgreSQL", icon: "devicon-postgresql-plain colored" },
+  { name: "React", icon: "devicon-react-original colored" },
+  { name: "Next.js", icon: "devicon-nextjs-plain" },
+  { name: "Vue.js", icon: "devicon-vuejs-plain colored" },
+  { name: "HTML5", icon: "devicon-html5-plain colored" },
+  { name: "CSS3", icon: "devicon-css3-plain colored" },
+  { name: "TailwindCSS", icon: "devicon-tailwindcss-original colored" },
   { name: "Docker", icon: "devicon-docker-plain colored" },
   { name: "GitHub", icon: "devicon-github-original" },
   { name: "Figma", icon: "devicon-figma-plain colored" },
@@ -23,7 +23,6 @@ const skills = [
 export default function Skills() {
   return (
     <section className="w-full max-w-3xl mx-auto text-center py-4">
-
       <div className="grid grid-cols-4 sm:grid-cols-5 md:grid-cols-7 gap-3">
         {skills.map((skill, i) => (
           <motion.div
